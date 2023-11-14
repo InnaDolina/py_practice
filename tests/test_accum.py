923601651fee3667e7b5633e2d2b7f05ceab7fef
Inna Dolina <dolina8inna@gmail.com>
2023-11-14 21:12:32 +0100

    explored pytest features

--- tests/test_accum.py
+++ tests/test_accum.py
@@ -1,34 +1,32 @@
 import pytest
 
-from stuff.accum import Accumulator
 
-
-def test_accumulator_init():
-    accum = Accumulator()
+@pytest.mark.accumulator
+def test_accumulator_init(accum):
     assert  accum.count == 0
 
 
-def test_accumulator_add_one():
-    accum = Accumulator()
+@pytest.mark.accumulator
+def test_accumulator_add_one(accum):
     accum.__add__(1)
     assert  accum.count == 1
 
 
-def test_accumulator_add_three():
-    accum = Accumulator()
+@pytest.mark.accumulator
+def test_accumulator_add_three(accum):
     accum.__add__(3)
     assert  accum.count == 3
 
 
-def test_accumulator_add_twice():
-    accum = Accumulator()
+@pytest.mark.accumulator
+def test_accumulator_add_twice(accum, accum2):
     accum.__add__(1)
     accum.__add__(1)
     assert  accum.count == 2
 
 
-def test_accumulator_cannot_set_count_directly():
-    accum = Accumulator()
+@pytest.mark.accumulator
+def test_accumulator_cannot_set_count_directly(accum):
     with pytest.raises(AttributeError, match=r"property 'count' of 'Accumulator' object has no setter"):
         accum.count = 10
 
